14c24a3e83fe7bb2486c02af4ab14e75104a5e1f
kangand <andrea.kang@gmail.com>
2020-07-09 12:26:29 -0400

    Methodology update

--- server.py
+++ server.py
@@ -1,52 +1,41 @@
-import os
 import numpy as np
-import pandas as pd
+from flask import Flask, request, render_template
+import pickle
 
-from flask import Flask, redirect, url_for, request, render_template
-from skimage import io
-import skimage.transform as st
+from fastai.vision import *
+from fastai import *
 
-from fastai.vision.learner import load_learner
-from fastai.vision.models import squeezenet1_0
-from fastai.vision.image import open_image
+import os
 
+cwd = os.getcwd()
+path = Path()
 
+application = Flask(__name__)
+
+model = load_learner('model/', 'squeezenet_model.pkl')
 
 app = Flask(__name__)
 
 @app.route('/')
-def entry_page():
+def hello_world():
     return render_template('index.html')
 
-@app.route('/predict_object/', methods=['GET', 'POST'])
-def render_message():
-    #Loading CNN model
-    saved_model = 'model/squeezenet_model'
-    model = load_learner(saved_model)
-    
-    try:
-        #Get image URL as input
-        image_url = request.form['image_url']
-        image = io.imread(image_url)
-        
-        #Apply same preprocessing used while training CNN model
-        image_small = st.resize(image, (224,224,3))
-        x = np.expand_dims(image_small.transpose(2, 0, 1), axis=0)
-        
-        #Call classify function to predict the image class using the loaded CNN model
-        pred = model.predict(image_small)
-        print(pred[0])
-        
-        #Store model prediction results to pass to the web page
-        message = "Model prediction: {}".format(pred[0])
-        print('Python module executed successfully')
-        
-    except Exception as e:
-        #Store error to pass to the web page
-        message = "Error encountered. Try another image. ErrorClass: {}, Argument: {} and Traceback details are: {}".format(e.__class__,e.args,e.__doc__)
-        final = pd.DataFrame({'A': ['Error'], 'B': [0]})
-        
-    #Return the model results to the web page
-    return render_template('index.html',
-                            message=message,
-                            image_url=image_url)
+@app.route('/predict',methods=['POST'])
+def predict():
+    #labels = ['grizzly','black','teddy']
+
+    file = request.files['file']
+
+    #Store the uploaded images in a temporary folder
+    if file:
+        filename = file.filename
+        file.save(os.path.join("resources/tmp", filename))
+
+    to_predict = "resources/tmp/"+filename
+    img = open_image(to_predict)
+
+    #Getting the prediction from the model
+    prediction = model.predict(img)[0]
+
+    #Render the result in the html template
+    return render_template('index.html', prediction_text='Your Prediction :  {} '.format(prediction))
